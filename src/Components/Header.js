e137b58dd1200de701d213c958a7425bd380cb53
Ivonne Hernandez <ivonne702@gmail.com>
2021-12-03 11:38:42 -0700

    Add styling and an svg to the Header component

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import '../styles/Header.css'
+import clapper from '../movie_clapper.svg';
 
 function Header() {
   return (
     <div className='header'>
-      <h1>Box Office Deetz</h1>
+      <h1>Box Office Deetz
+        <img src={clapper} className="clapper"  alt="clapper"/>
+      </h1>
       <input type="text" placeholder="Ready, set, search"></input>
     </div>
   )
