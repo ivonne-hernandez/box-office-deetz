39bc2ed16ad74cd043c226cf966d4244d4fbbacb
Ivonne Hernandez <ivonne702@gmail.com>
2021-12-02 17:36:34 -0700

    Remove unused import statement

--- src/Components/MovieCard.js
+++ src/Components/MovieCard.js
@@ -4,7 +4,7 @@ const MovieCard = ({ title, poster, backdrop, averageRating, releaseDate, id }) 
   return (
     <article className="movieCard">
       <h2>{title}</h2>
-      {/* <img src={poster}/> */}
+      {/* <img src={poster} alt={ title + `poster`}/> */}
       {/* <img src={backdrop} /> */}
       <p>Average Rating: {Number(averageRating.toFixed(2))}</p>
       <p>Release Date: {releaseDate}</p>
