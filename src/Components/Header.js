746546cc58bf790d415cb16c196a485220aec902
Ivonne Hernandez <ivonne702@gmail.com>
2021-12-03 06:49:01 -0700

    Add styling to MovieCard, Header, App and MoviesContainer components

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import '../styles/Header.css'
 
 function Header() {
   return (
     <div className='header'>
       <h1>Box Office Deetz</h1>
+      <input type="text" placeholder="Ready, set, search"></input>
     </div>
   )
 }
--- src/Components/MovieCard.js
+++ src/Components/MovieCard.js
@@ -5,9 +5,9 @@ import '../styles/MovieCard.css'
 const MovieCard = ({ title, poster, backdrop, averageRating, releaseDate, id }) => {
   return (
     <article className="movieCard">
-      <h2>{title}</h2>
       {<img src={poster} alt={ title + ` poster`} className='movieCardPoster'/>}
-      {/* <img src={backdrop} /> */}
+      <h2>{title}</h2>
+      {/* <img src={backdrop} className='movieCardBackDrop'/> */}
       <p>Average Rating: {Number(averageRating.toFixed(2))}</p>
       <p>Release Date: {releaseDate}</p>
 
