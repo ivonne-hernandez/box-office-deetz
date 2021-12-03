62785ad46dcc4efc81497eb88903fc979f07d84c
Colgan Meanor <colganmeanor7@gmail.com>
2021-12-02 19:43:35 -0600

    Add basic styling to movie display

--- src/Components/MovieCard.js
+++ src/Components/MovieCard.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
+import '../styles/MovieCard.css'
+
 const MovieCard = ({ title, poster, backdrop, averageRating, releaseDate, id }) => {
   return (
     <article className="movieCard">
       <h2>{title}</h2>
-      {/* <img src={poster} alt={ title + `poster`}/> */}
+      {<img src={poster} alt={ title + ` poster`} className='movieCardPoster'/>}
       {/* <img src={backdrop} /> */}
       <p>Average Rating: {Number(averageRating.toFixed(2))}</p>
       <p>Release Date: {releaseDate}</p>
