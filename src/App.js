7cbdef4439432ce6703c2313b36a3aa73945404f
Ivonne Hernandez <ivonne702@gmail.com>
2021-12-03 18:07:14 -0700

    Add isModalOpen, selectedMovieId properties to state as well as toggleModal and setSelecteMovidId methods and pass them down as props to MovieContainer

--- src/App.js
+++ src/App.js
@@ -2,23 +2,32 @@ import React, { Component } from 'react';
 import movieData from './movieData';
 import Header from './Components/Header';
 import MoviesContainer from './Components/MoviesContainer';
-
-
 import './App.css';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      movies: movieData.movies
+      movies: movieData.movies,
+      isModalOpen: false,
+      selectedMovieId: null
     }
+  }
+
+  toggleModal = (bool) => {
+    console.log('bool', bool);
+    this.setState({isModalOpen: bool});
+  }
+
+  setSelectedMovieId = (id) => {
+    this.setState({selectedMovieId: id});
   }
 
   render() {
     return (
       <div className="App">
         <Header />
-        <MoviesContainer movies={this.state.movies} />
+        <MoviesContainer movies={this.state.movies} isModalOpen={this.state.isModalOpen} toggleModal={this.toggleModal} selectedMovieId={this.state.selectedMovieId} setSelectedMovieId={this.setSelectedMovieId}/>
       </div>
     );
   }
