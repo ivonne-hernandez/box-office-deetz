75c48ef2ffe0333dc7e0358064e3aa41cc247961
Colgan Meanor <colganmeanor7@gmail.com>
2021-12-05 16:16:45 -0600

    Refactor modal component into class based component and hold state

--- src/Components/Modal.js
+++ src/Components/Modal.js
@@ -19,33 +19,58 @@ class Modal extends React.Component {
       genres: null,
       budget: null,
       revenue: null,
-      tagline: null
+      tagline: null,
+      isModalLoading: true,
     }
   }
 
 
 componentDidMount = () => {
   fetchSingleMovie(this.state.movie)
+  .then((data) => this.setState({
+    title: data.movie.title,
+    poster: data.movie.poster_path,
+    backdrop: data.movie.backdrop_path,
+    averageRating: Number(data.movie.average_rating).toFixed(2),
+    releaseDate: data.movie.release_date,
+    overview: data.movie.overview,
+    runtime: data.movie.runtime,
+    genres: data.movie.genres,
+    budget: data.movie.budget,
+    revenue: data.movie.revenue,
+    tagline: data.movie.tagline,
+    isModalLoading: false
+  }))
 }
 
 render = () => {
+  const myStyle = {
+        backgroundImage: `url(${this.state.backdrop})`
+      }
 
-  if (this.props.isModalOpen && this.props.selectedMovie === this.props.id) {
-    return ReactDOM.createPortal(
-      <div className = "modal" title={this.props.title}>
-        <div className="modal-window">
-          <img src={this.props.backdrop} className="modal-backdrop-img"></img>
-          <h2>{this.props.title}</h2>
-          <p>TagLine</p>
-
-          <button onClick={() => this.props.toggleModal(false)}>Close</button>
-        </div>
+  return ReactDOM.createPortal(
+      <div className="modal">
+        <article className="modal-window">
+          <div className="modal-backdrop-img" style={myStyle}>
+          </div>
+          <img src={this.state.poster} alt={this.state.title + ` poster`} className="modal-poster"/>
+          <div className="modal-poster movie-content">
+            <div className="modal-title">
+              <h3>{this.state.title}</h3>
+            </div>
+            <p className="modal-p"><b>Average Rating:</b> {this.state.averageRating} / 10</p>
+            <p className="modal-p"><b>Release Date:</b> {this.state.releaseDate}</p>
+            <p className="modal-p"><b>Overview:</b> {this.state.overview}</p>
+            <p className="modal-p"><b>Genres:</b> {this.state.genres}</p>
+            <p className="modal-p"><b>Budget:</b> {this.state.budget}</p>
+            <p className="modal-p"><b>Revenue:</b> {this.state.revenue}</p>
+            <p className="modal-p"><b>Tagline:</b> {this.state.tagline}</p>
+            <button onClick={() => this.props.toggleModal(false)}>Close</button>
+          </div>
+        </article>
       </div>,
       document.getElementById('modal-container')
     )
-  } else {
-    return null;
-  }
 }
 
 }
