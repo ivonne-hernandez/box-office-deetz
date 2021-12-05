03e8a9028c643fdb3817408cb0a178a64f8813d1
Ivonne Hernandez <ivonne702@gmail.com>
2021-12-05 11:21:38 -0700

    Add a fetchSingleMovie function to api-Calls and import it to Modal.js

--- src/Components/Modal.js
+++ src/Components/Modal.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { fetchSingleMovie } from '../api-Calls';
 
 import '../styles/Modal.css'
 
@@ -26,23 +27,23 @@ const Modal = ({
   if (isModalOpen && selectedMovie === id) {
     return ReactDOM.createPortal(
       <div className="modal">
-          <article className="modal-window">
-            <div className="modal-backdrop-img" style={myStyle}>
+        <article className="modal-window">
+          <div className="modal-backdrop-img" style={myStyle}>
+          </div>
+          <img src={poster} alt={title + ` poster`} className="modal-poster"/>
+          <div className="modal-poster movie-content">
+            <div className="modal-title">
+              <h3>{title}</h3>
             </div>
-            <img src={poster} alt={title + ` poster`} className="modal-poster"/>
-            <div className="modal-poster movie-content">
-              <div className="modal-title">
-                <h3>{title}</h3>
-              </div>
-              <p className="modal-p"><b>Average Rating:</b> {Number(averageRating.toFixed(2))} / 10</p>
-              <p className="modal-p"><b>Release Date:</b> {releaseDate}</p>
-              <p className="modal-p"><b>Overview:</b> {overview}</p>
-              <p className="modal-p"><b>Genres:</b> {genres}</p>
-              <p className="modal-p"><b>Budget:</b> {budget}</p>
-              <p className="modal-p"><b>Revenue:</b> {revenue}</p>
-              <p className="modal-p"><b>Tagline:</b> {tagline}</p>
-              <button onClick={() => toggleModal(false)}>Close</button>
-            </div>
+            <p className="modal-p"><b>Average Rating:</b> {Number(averageRating.toFixed(2))} / 10</p>
+            <p className="modal-p"><b>Release Date:</b> {releaseDate}</p>
+            <p className="modal-p"><b>Overview:</b> {overview}</p>
+            <p className="modal-p"><b>Genres:</b> {genres}</p>
+            <p className="modal-p"><b>Budget:</b> {budget}</p>
+            <p className="modal-p"><b>Revenue:</b> {revenue}</p>
+            <p className="modal-p"><b>Tagline:</b> {tagline}</p>
+            <button onClick={() => toggleModal(false)}>Close</button>
+          </div>
         </article>
       </div>,
       document.getElementById('modal-container')
@@ -50,8 +51,6 @@ const Modal = ({
   } else {
     return null;
   }
-
-
 }
 
 export default Modal
