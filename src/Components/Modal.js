315b301c124fdcfbbe72413dab8f38518ffa30ec
Colgan Meanor <colganmeanor7@gmail.com>
2021-12-05 14:12:50 -0600

    Adjust props syntax in Modal.js

--- src/Components/Modal.js
+++ src/Components/Modal.js
@@ -3,54 +3,50 @@ import ReactDOM from 'react-dom';
 
 import '../styles/Modal.css'
 
-const Modal = ({
-  title,
-  poster,
-  backdrop,
-  averageRating,
-  releaseDate,
-  overview,
-  genres,
-  budget, 
-  revenue, 
-  tagline,
-  id,
-  selectedMovie,
-  isModalOpen,
-  toggleModal
-}) => {
-  const myStyle = {
-    backgroundImage: `url(${backdrop})`
+import { fetchSingleMovie } from '../api-Calls'
+
+class Modal extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      movie: this.props.selectedMovie,
+      title: null,
+      poster: null,
+      backdrop: null,
+      averageRating: null,
+      releaseDate: null,
+      overview: null,
+      genres: null,
+      budget: null,
+      revenue: null,
+      tagline: null
+    }
   }
 
-  if (isModalOpen && selectedMovie === id) {
+
+componentDidMount = () => {
+  fetchSingleMovie(this.state.movie)
+}
+
+render = () => {
+
+  if (this.props.isModalOpen && this.props.selectedMovie === this.props.id) {
     return ReactDOM.createPortal(
-      <div className="modal">
-          <article className="modal-window">
-            <div className="modal-backdrop-img" style={myStyle}>
-            </div>
-            <img src={poster} alt={title + ` poster`} className="modal-poster"/>
-            <div className="modal-poster movie-content">
-              <div className="modal-title">
-                <h3>{title}</h3>
-              </div>
-              <p className="modal-p"><b>Average Rating:</b> {Number(averageRating.toFixed(2))} / 10</p>
-              <p className="modal-p"><b>Release Date:</b> {releaseDate}</p>
-              <p className="modal-p"><b>Overview:</b> {overview}</p>
-              <p className="modal-p"><b>Genres:</b> {genres}</p>
-              <p className="modal-p"><b>Budget:</b> {budget}</p>
-              <p className="modal-p"><b>Revenue:</b> {revenue}</p>
-              <p className="modal-p"><b>Tagline:</b> {tagline}</p>
-              <button onClick={() => toggleModal(false)}>Close</button>
-            </div>
-        </article>
+      <div className = "modal" title={this.props.title}>
+        <div className="modal-window">
+          <img src={this.props.backdrop} className="modal-backdrop-img"></img>
+          <h2>{this.props.title}</h2>
+          <p>TagLine</p>
+
+          <button onClick={() => this.props.toggleModal(false)}>Close</button>
+        </div>
       </div>,
       document.getElementById('modal-container')
     )
   } else {
     return null;
   }
-
+}
 
 }
 
--- src/Components/MovieCard.js
+++ src/Components/MovieCard.js
@@ -10,30 +10,30 @@ const MovieCard = ({ title, poster, backdrop, averageRating, releaseDate, overvi
   }
 
   return (
-    <>
+    // <>
       <article className="movieCard" onClick={() => clickMovie()}>
         {<img src={poster} alt={ title + ` poster`} className='movieCardPoster'/>}
         <h2>{title}</h2>
         <p>Average Rating: {Number(averageRating.toFixed(2))} / 10</p>
         <p>Release Date: {releaseDate}</p>
       </article>
-      <Modal
-        title={title}
-        poster={poster}
-        backdrop={backdrop}
-        averageRating={averageRating}
-        releaseDate={releaseDate}
-        overview={overview}
-        genres={genres}
-        budget={budget}
-        revenue={revenue}
-        tagline={tagline}
-        id={id}
-        selectedMovie={selectedMovie}
-        isModalOpen={isModalOpen}
-        toggleModal={toggleModal}
-        />
-    </>
+      // <Modal
+      //   title={title}
+      //   poster={poster}
+      //   backdrop={backdrop}
+      //   averageRating={averageRating}
+      //   releaseDate={releaseDate}
+      //   overview={overview}
+      //   genres={genres}
+      //   budget={budget}
+      //   revenue={revenue}
+      //   tagline={tagline}
+      //   id={id}
+      //   selectedMovie={selectedMovie}
+      //   isModalOpen={isModalOpen}
+      //   toggleModal={toggleModal}
+      //   />
+    // </>
   )
 }
 
