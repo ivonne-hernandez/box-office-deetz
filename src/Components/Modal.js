29fb29dac08ff3239eccd38a76a1f167fcd7ab53
Ivonne Hernandez <ivonne702@gmail.com>
2021-12-07 15:23:43 -0700

    Restyle the modal component by refactoring the modal container to a class

--- src/Components/Modal.js
+++ src/Components/Modal.js
@@ -27,25 +27,27 @@ render = () => {
       backgroundImage: `url(${this.state.movie.backdrop_path})`
     }
     return (
-      <div className="modal">
-        <article className="modal-window">
-          <div className="modal-backdrop-img" style={myStyle}>
-          </div>
-          <img src={this.state.movie.poster_path} alt={this.state.movie.title + ` poster`} className="modal-poster"/>
-          <div className="modal-poster movie-content">
-            <div className="modal-title">
-              <h3>{this.state.movie.title}</h3>
+      <div className="modal-container">
+        <div className="modal">
+          <article className="modal-window">
+            <div className="modal-backdrop-img" style={myStyle}>
             </div>
-            <p className="modal-p"><b>Average Rating:</b> {this.state.movie.average_rating} / 10</p>
-            <p className="modal-p"><b>Release Date:</b> {this.state.movie.release_date}</p>
-            <p className="modal-p"><b>Overview:</b> {this.state.movie.overview}</p>
-            <p className="modal-p"><b>Genres:</b> {this.state.movie.genres}</p>
-            <p className="modal-p"><b>Budget:</b> {this.state.movie.budget}</p>
-            <p className="modal-p"><b>Revenue:</b> {this.state.movie.revenue}</p>
-            <p className="modal-p"><b>Tagline:</b> {this.state.movie.tagline}</p>
-            <button onClick={() => this.props.toggleModal(false)}>Close</button>
-          </div>
-        </article>
+            <img src={this.state.movie.poster_path} alt={this.state.movie.title + ` poster`} className="modal-poster"/>
+            <div className="modal-poster movie-content">
+              <div className="modal-title">
+                <h3>{this.state.movie.title}</h3>
+              </div>
+              <p className="modal-p"><b>Average Rating:</b> {this.state.movie.average_rating} / 10</p>
+              <p className="modal-p"><b>Release Date:</b> {this.state.movie.release_date}</p>
+              <p className="modal-p"><b>Overview:</b> {this.state.movie.overview}</p>
+              <p className="modal-p"><b>Genres:</b> {this.state.movie.genres}</p>
+              <p className="modal-p"><b>Budget:</b> {this.state.movie.budget}</p>
+              <p className="modal-p"><b>Revenue:</b> {this.state.movie.revenue}</p>
+              <p className="modal-p"><b>Tagline:</b> {this.state.movie.tagline}</p>
+              <button onClick={() => this.props.toggleModal(false)}>Close</button>
+            </div>
+          </article>
+        </div>
       </div>
     )
   } else {
