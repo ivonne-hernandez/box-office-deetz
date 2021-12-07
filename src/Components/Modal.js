da82cbbbc7568146ce1beb8877935912a2de5ed9
Ivonne Hernandez <ivonne702@gmail.com>
2021-12-07 14:39:45 -0700

    Refactor the state for Modal and add conditional rendering

--- src/Components/Modal.js
+++ src/Components/Modal.js
@@ -1,78 +1,59 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-import '../styles/Modal.css'
-
-import { fetchSingleMovie } from '../api-Calls'
+import '../styles/Modal.css';
+import { fetchSingleMovie } from '../api-Calls';
 
 class Modal extends React.Component {
-  constructor(props) {
-    super(props);
+  constructor() {
+    super();
     this.state = {
-      movie: this.props.selectedMovie,
-      title: null,
-      poster: null,
-      backdrop: null,
-      averageRating: null,
-      releaseDate: null,
-      overview: null,
-      genres: null,
-      budget: null,
-      revenue: null,
-      tagline: null,
+      movie: null,
       isModalLoading: true,
     }
   }
 
-
 componentDidMount = () => {
-  fetchSingleMovie(this.state.movie)
-  .then((data) => this.setState({
-    title: data.movie.title,
-    poster: data.movie.poster_path,
-    backdrop: data.movie.backdrop_path,
-    averageRating: Number(data.movie.average_rating).toFixed(2),
-    releaseDate: data.movie.release_date,
-    overview: data.movie.overview,
-    runtime: data.movie.runtime,
-    genres: data.movie.genres,
-    budget: data.movie.budget,
-    revenue: data.movie.revenue,
-    tagline: data.movie.tagline,
-    isModalLoading: false
-  }))
+  fetchSingleMovie(this.props.selectedMovie)
+    .then(data => this.setState({
+      movie: data.movie,
+      isModalLoading: false
+    }))
 }
 
-render = () => {
-  const myStyle = {
-        backgroundImage: `url(${this.state.backdrop})`
-      }
 
-  return ReactDOM.createPortal(
+render = () => {
+  if (this.state.movie && !this.state.isModalLoading) {
+    const myStyle = {
+      backgroundImage: `url(${this.state.movie.backdrop_path})`
+    }
+    return ReactDOM.createPortal(
       <div className="modal">
         <article className="modal-window">
           <div className="modal-backdrop-img" style={myStyle}>
           </div>
-          <img src={this.state.poster} alt={this.state.title + ` poster`} className="modal-poster"/>
+          <img src={this.state.movie.poster_path} alt={this.state.movie.title + ` poster`} className="modal-poster"/>
           <div className="modal-poster movie-content">
             <div className="modal-title">
-              <h3>{this.state.title}</h3>
+              <h3>{this.state.movie.title}</h3>
             </div>
-            <p className="modal-p"><b>Average Rating:</b> {this.state.averageRating} / 10</p>
-            <p className="modal-p"><b>Release Date:</b> {this.state.releaseDate}</p>
-            <p className="modal-p"><b>Overview:</b> {this.state.overview}</p>
-            <p className="modal-p"><b>Genres:</b> {this.state.genres}</p>
-            <p className="modal-p"><b>Budget:</b> {this.state.budget}</p>
-            <p className="modal-p"><b>Revenue:</b> {this.state.revenue}</p>
-            <p className="modal-p"><b>Tagline:</b> {this.state.tagline}</p>
+            <p className="modal-p"><b>Average Rating:</b> {this.state.movie.average_rating} / 10</p>
+            <p className="modal-p"><b>Release Date:</b> {this.state.movie.release_date}</p>
+            <p className="modal-p"><b>Overview:</b> {this.state.movie.overview}</p>
+            <p className="modal-p"><b>Genres:</b> {this.state.movie.genres}</p>
+            <p className="modal-p"><b>Budget:</b> {this.state.movie.budget}</p>
+            <p className="modal-p"><b>Revenue:</b> {this.state.movie.revenue}</p>
+            <p className="modal-p"><b>Tagline:</b> {this.state.movie.tagline}</p>
             <button onClick={() => this.props.toggleModal(false)}>Close</button>
           </div>
         </article>
       </div>,
       document.getElementById('modal-container')
     )
+  } else {
+    return null;
+  }
 }
 
 }
 
-export default Modal
+export default Modal;
