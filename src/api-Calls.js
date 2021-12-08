692b5060823a89b8b8cf399d2ccedcd70b807e9c
Ivonne Hernandez <ivonne702@gmail.com>
2021-12-08 09:34:22 -0700

    Add a throw new Error instance to the fetchSingleMovie function if the response is not okay

--- src/api-Calls.js
+++ src/api-Calls.js
@@ -9,9 +9,13 @@ const fetchAllMovies = () => {
 }
 
 const fetchSingleMovie = (id) => {
-  return fetch(`https://rancid-tomatillos.herokuapp.com/api/v2/movies/${id}`)
-    .then((response) => response.json())
-    .catch((err) => console.log(err))
+  return fetch(`https://rancid-tomatillos.herokuapp.com/api/v2/movies/${id}/potatoes`)
+    .then(response => {
+      if(!response.ok) {
+        throw new Error(`${response.status}: ${response.statusText}`);
+      }
+      return response.json();
+    });
 }
 
 module.exports = {
