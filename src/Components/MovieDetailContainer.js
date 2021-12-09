d0018ff49c44484ec928f1c331826ed11571bdfd
Ivonne Hernandez <ivonne702@gmail.com>
2021-12-09 16:27:46 -0700

    Import useNavigate and useParams, create a closeMovieDetails function to pass as props to the MovieDetails component

--- src/Components/MovieDetailContainer.js
+++ src/Components/MovieDetailContainer.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import MovieDetails from './MovieDetails';
+import { useNavigate } from 'react-router';
+import { useParams } from 'react-router-dom';
 
-const MovieDetailContainer = ({ selectedMovie, resetSelectedMovie }) => {
-    
+const MovieDetailContainer = ({ resetSelectedMovie }) => {
+  let navigate = useNavigate();
+  let { id } = useParams();
+  
+  const closeMovieDetails = () => {
+    resetSelectedMovie();
+    navigate("/");
+  }
+  
   return (
     <MovieDetails
-    selectedMovie={selectedMovie}
-    resetSelectedMovie={resetSelectedMovie}
+      selectedMovie={id}
+      closeMovieDetails={closeMovieDetails}
     />
   );
 }
