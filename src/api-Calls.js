eed9af275c490282c3838f61cea85945b6bce051
Ivonne Hernandez <ivonne702@gmail.com>
2021-12-11 10:20:37 -0700

    Remove the test path from the apiCalls file

--- src/api-Calls.js
+++ src/api-Calls.js
@@ -9,7 +9,7 @@ const fetchAllMovies = () => {
 }
 
 const fetchSingleMovie = (id) => {
-  return fetch(`https://rancid-tomatillos.herokuapp.com/api/v2/movies/${id}/potatoes`)
+  return fetch(`https://rancid-tomatillos.herokuapp.com/api/v2/movies/${id}`)
     .then(response => {
       if(!response.ok) {
         throw new Error(`${response.status} ${response.statusText}`);
