6b5efe186e6c4453f829c5afc0e79806c0da8a15
Ivonne Hernandez <ivonne702@gmail.com>
2021-12-11 15:35:11 -0700

    Refactor indentation and add semicolons to it-blocks

--- cypress/integration/dashboard_spec.js
+++ cypress/integration/dashboard_spec.js
@@ -1,87 +1,84 @@
 describe('Box Office Deetz Test', () => {
-
     beforeEach(() => {
-        cy.intercept('GET', '/api/v2/movies', {
-            fixture: 'allMovies.json'
-        })
-        cy.visit('http://localhost:3000');
+      cy.intercept('GET', '/api/v2/movies', {
+          fixture: 'allMovies.json'
+      })
+      cy.visit('http://localhost:3000');
     });
 
     it('the header should contain the name of the application and the clapper logo image', () => {
-        cy.get('div[class="header"]')
-            .get('img')
-            .get('h1[class="headerTitle"]')
-            .contains('Box Office Deetz')
+      cy.get('div[class="header"]')
+        .get('img')
+        .get('h1[class="headerTitle"]')
+        .contains('Box Office Deetz')
     });
 
     it('should have a container component for all movie cards', () => {
-        cy.get('main[class="movieContainer"]')
-            .children('article[class="movieCard"]')
-            .children('img')
-            .siblings('h2')
-            .siblings('p[class="movieCardRating"]')
-            .siblings('p[class="movieCardReleaseDate"]')
-    })
+      cy.get('main[class="movieContainer"]')
+        .children('article[class="movieCard"]')
+        .children('img')
+        .siblings('h2')
+        .siblings('p[class="movieCardRating"]')
+        .siblings('p[class="movieCardReleaseDate"]')
+    });
 
     it('Should be able to load a movieDetails page', () => {
-        cy.get('article[id=337401]')
-            .click()
-            .intercept('GET', '/api/v2/movies/337401', {
-                fixture: 'sampleMovie.json'
-            })
-            .get('div[class="modal"]')
-            .contains("Mulan")
-    })
+      cy.get('article[id=337401]')
+        .click()
+        .intercept('GET', '/api/v2/movies/337401', {
+            fixture: 'sampleMovie.json'
+        })
+        .get('div[class="modal"]')
+        .contains("Mulan")
+    });
 
     it('Should be able to click the close button in the Movie Details page and return to the Home page', () => {
-        cy.visit('http://localhost:3000/337401')
-            .get('button[class="movie-details-close-button"]')
-            .click()
-            .url('http://localhost:3000')
-    })
+      cy.visit('http://localhost:3000/337401')
+        .get('button[class="movie-details-close-button"]')
+        .click()
+        .url('http://localhost:3000')
+    });
 
     it('Should be able to click on the back and forward buttons in the browser to navigate to previously selected paths', () => {
-        cy.visit('http://localhost:3000/')
-            .get('article[id=337401]')
-            .click()
-            .url('http://localhost:3000/337401')
-            .go('back')
-            .url('http://localhost:3000')
-            .go('forward')
-            .url('http://localhost:3000/337401')
-    })
+      cy.visit('http://localhost:3000/')
+        .get('article[id=337401]')
+        .click()
+        .url('http://localhost:3000/337401')
+        .go('back')
+        .url('http://localhost:3000')
+        .go('forward')
+        .url('http://localhost:3000/337401')
+    });
 
     it('Should be able to load the correct movie details page given a specific URL', () => {
-        cy.visit('http://localhost:3000/337401')
-            .get('div[class="modal-backdrop-img"]')
-            .get('img[alt="Mulan poster"]')
-            .get('div[class="modal-title"]')
-            .get('p[id="average-rating"]')
-
-
-    })
+      cy.visit('http://localhost:3000/337401')
+        .get('div[class="modal-backdrop-img"]')
+        .get('img[alt="Mulan poster"]')
+        .get('div[class="modal-title"]')
+        .get('p[id="average-rating"]')
+    });
 
     it('Should be able to load the home page when the user hits refresh', () => {
-        cy.reload()
-            .get('div[class="header"]')
-            .get('img')
-            .get('h1[class="headerTitle"]')
-            .contains('Box Office Deetz')
-            .get('main[class="movieContainer"]')
-            .children('article[class="movieCard"]')
-            .children('img[alt="Mulan poster"]')
-            .siblings('h2')
-            .siblings('p[class="movieCardRating"]')
-            .siblings('p[class="movieCardReleaseDate"]')
-    })
+      cy.reload()
+        .get('div[class="header"]')
+        .get('img')
+        .get('h1[class="headerTitle"]')
+        .contains('Box Office Deetz')
+        .get('main[class="movieContainer"]')
+        .children('article[class="movieCard"]')
+        .children('img[alt="Mulan poster"]')
+        .siblings('h2')
+        .siblings('p[class="movieCardRating"]')
+        .siblings('p[class="movieCardReleaseDate"]')
+    });
 
     it('Should be able to load the movie details page when the user hits refresh', () => {
-        cy.visit('http://localhost:3000/337401')
-            .reload()
-            .get('div[alt="Mulan backdrop img"]')
-            .get('img[alt="Mulan poster"]')
-            .get('div[class="modal-title"]')
-            .contains('Mulan')
-            .get('p[id="average-rating"]')
-    })
+      cy.visit('http://localhost:3000/337401')
+        .reload()
+        .get('div[alt="Mulan backdrop img"]')
+        .get('img[alt="Mulan poster"]')
+        .get('div[class="modal-title"]')
+        .contains('Mulan')
+        .get('p[id="average-rating"]')
+    });
 });
