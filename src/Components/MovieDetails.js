987ace87f82b66723fd7030cbc2d28ff458e6830
Colgan Meanor <colganmeanor7@gmail.com>
2021-12-12 14:41:24 -0600

    Renamed Modal.css to MovieDetails.css and updated all classnames to no longer refer to modal

--- src/Components/MovieDetails.js
+++ src/Components/MovieDetails.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import '../styles/Modal.css';
+import '../styles/MovieDetails.css';
 import { fetchSingleMovie } from '../api-Calls';
 
 class MovieDetails extends React.Component {
@@ -26,22 +26,22 @@ render = () => {
     }
     return (
       <div className="modal-container">
-        <div className="modal">
-          <article className="modal-window">
-            <div className="modal-backdrop-img" style={myStyle}>
+        <div className="movie-details">
+          <article className="movie-details-window">
+            <div className="movie-details-backdrop-img" style={myStyle}>
             </div>
-            <img src={this.state.movie.poster_path} alt={this.state.movie.title + ` poster`} className="modal-poster"/>
-            <div className="modal-poster movie-content">
-              <div className="modal-title">
+            <img src={this.state.movie.poster_path} alt={this.state.movie.title + ` poster`} className="movie-details-poster"/>
+            <div className="movie-details-poster movie-content">
+              <div className="movie-details-title">
                 <h3>{this.state.movie.title}</h3>
               </div>
-              <p className="modal-p"><b>Average Rating:</b> {this.state.movie.average_rating} / 10</p>
-              <p className="modal-p"><b>Release Date:</b> {this.state.movie.release_date}</p>
-              <p className="modal-p"><b>Overview:</b> {this.state.movie.overview}</p>
-              <p className="modal-p"><b>Genres:</b> {this.state.movie.genres}</p>
-              <p className="modal-p"><b>Budget:</b> {this.state.movie.budget}</p>
-              <p className="modal-p"><b>Revenue:</b> {this.state.movie.revenue}</p>
-              <p className="modal-p"><b>Tagline:</b> {this.state.movie.tagline}</p>
+              <p className="movie-details-p"><b>Average Rating:</b> {this.state.movie.average_rating} / 10</p>
+              <p className="movie-details-p"><b>Release Date:</b> {this.state.movie.release_date}</p>
+              <p className="movie-details-p"><b>Overview:</b> {this.state.movie.overview}</p>
+              <p className="movie-details-p"><b>Genres:</b> {this.state.movie.genres}</p>
+              <p className="movie-details-p"><b>Budget:</b> {this.state.movie.budget}</p>
+              <p className="movie-details-p"><b>Revenue:</b> {this.state.movie.revenue}</p>
+              <p className="movie-details-p"><b>Tagline:</b> {this.state.movie.tagline}</p>
               <button onClick={() => this.props.closeMovieDetails()}>Close</button>
             </div>
           </article>
