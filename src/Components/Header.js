35d51867490a4d264f1a50d85b1071951426e71d
Colgan Meanor <colganmeanor7@gmail.com>
2021-12-12 17:58:00 -0600

    Add NavLink component to /favorites in Header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom'
 import '../styles/Header.css'
 import clapper from '../movie_clapper.svg';
 
@@ -6,6 +7,7 @@ function Header() {
   return (
     <div className='header'>
       <h1>Box Office Deetz</h1>
+      <NavLink to="/favorites">Favorite Movies</NavLink>
       <img src={clapper} className="clapper"  alt="clapper"/>
     </div>
   )
