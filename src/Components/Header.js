72a2c4af3e045d53041b80561901d62679fccb3a
Colgan Meanor <colganmeanor7@gmail.com>
2021-12-12 18:37:27 -0600

    Add conditional rendering in Header to determine what NavLink is shown, Home or Favorites

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -2,12 +2,22 @@ import React from 'react';
 import { NavLink } from 'react-router-dom'
 import '../styles/Header.css'
 import clapper from '../movie_clapper.svg';
+import {useLocation} from 'react-router-dom'
+
+
 
 function Header() {
+
+  const location = useLocation()
+  console.log(location)
+
+  let headerLink  
+  location.pathname === '/favorites' ? headerLink = <NavLink to="/">Home</NavLink> : headerLink = <NavLink to="/favorites">Favorite Movies</NavLink>
+
   return (
     <div className='header'>
       <h1>Box Office Deetz</h1>
-      <NavLink to="/favorites">Favorite Movies</NavLink>
+      {headerLink}
       <img src={clapper} className="clapper"  alt="clapper"/>
     </div>
   )
