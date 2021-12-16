b43cbe2ee38760b4a6a59ac32be24ffad2c46c2a
Ivonne Hernandez <ivonne702@gmail.com>
2021-12-16 16:07:53 -0700

    Refactor the Header component to have an onClick function

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,14 +1,18 @@
 import React from 'react';
-import { Link, useLocation } from 'react-router-dom';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 import '../styles/Header.css';
 
 const Header = () => {
   const location = useLocation();
+  const navigate = useNavigate();
 
   return (
     <div className='header'>
-      <h1>Box Office Deetz</h1>
-      {location.pathname === '/favorites' ? <Link to="/" className="home-link">Home</Link> : 
+      <h1
+        onClick={() => navigate("/")}>
+      Box Office Deetz
+      </h1>
+      {location.pathname === "/favorites" ? <Link to="/" className="home-link">Home</Link> : 
         <Link to="/favorites" className="favorite-movies-link">Favorite Movies</Link>}
     </div>
   )
